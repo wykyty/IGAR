abeb1caf799f87ba29537968f4c1a8a223b41acf
Wang Yikang <wangyikang@sdu.edu.cn>
2026-03-11 19:25:20 +0800

    feat: 加载jsonl文件 成功跑通流程

--- src/test_basic_example.py
+++ src/test_basic_example.py
@@ -33,21 +33,25 @@ config.set_provider_config("embedding", "OpenAIEmbedding", {
     "api_key": api_key
 })
 
+# 配置 FileLoader
+config.set_provider_config("file_loader", "JsonFileLoader", {
+    "text_key": "content"
+})
+
 init_config(config=config)
 
-# You should clone the milvus docs repo to your local machine first, execute:
-# git clone https://github.com/milvus-io/milvus-docs.git
-# Then replace the path below with the path to the milvus-docs repo on your local machine
-# import glob
-# all_md_files = glob.glob('xxx/milvus-docs/site/en/**/*.md', recursive=True)
-# load_from_local_files(paths_or_directory=all_md_files, collection_name="milvus_docs", collection_description="All Milvus Documents")
-
 # Hint: You can also load a single file, please execute it in the root directory of the deep searcher project
+# load_from_local_files(
+#     paths_or_directory=os.path.join(workspace, "data/pdfs/Lixiang2023AnnualReport.pdf"),
+#     collection_name="Lixiang_docs",
+#     collection_description="Lixiang2023 Documents",
+#     # force_new_collection=True, # If you want to drop origin collection and create a new collection every time, set force_new_collection to True
+# )
+
 load_from_local_files(
-    paths_or_directory=os.path.join(workspace, "data/pdfs/Lixiang2023AnnualReport.pdf"),
-    collection_name="Lixiang_docs",
-    collection_description="Lixiang2023 Documents",
-    # force_new_collection=True, # If you want to drop origin collection and create a new collection every time, set force_new_collection to True
+    paths_or_directory=os.path.join(workspace, "data/corpus_with_docids.jsonl"),
+    collection_name="LiAuto",
+    collection_description="Lixiang2023 doc",
 )
 
 # question = "Write a report comparing Milvus with other vector databases."
